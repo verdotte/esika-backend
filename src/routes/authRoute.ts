b32f48dc971b20eccc15cd92e935b2a9d29f1c56
Desire Kaleba <desirekaleba@gmail.com>
2021-07-06 13:49:13 +0300

    feat: add auth middleware

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,16 +2,20 @@ import { Router } from 'express';
 import { IRoute } from '../interfaces/route.interface';
 import { AuthController } from '../controllers/authController';
 import { asyncHandler } from '../middlewares';
+import { adminValidator } from '../validators/adminValidator';
 
 export class AuthRoute implements IRoute {
   public path = '/auth';
   public router = Router();
 
-  constructor(private readonly auth: AuthController) {
+  constructor(private readonly authController: AuthController) {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.route(this.path).post(asyncHandler(this.auth.login));
+    this.router
+      .route(`${this.path}/login`)
+      .post(adminValidator.login,
+        asyncHandler(this.authController.login));
   }
 }
