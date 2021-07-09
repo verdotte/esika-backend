1d08674f81347cd77ac2988abebeb426ba36be93
Desire Kaleba <desirekaleba@gmail.com>
2021-07-09 22:04:33 +0300

    feat: add test dummy data

--- src/__mocks__/dummyData.ts
+++ src/__mocks__/dummyData.ts
@@ -1,6 +1,22 @@
-export const adminData = {
+import { AdminRole } from '../database/entity/Admin';
+import { CreateAdminDto } from '../dtos/createAdminDto';
+import { LoginAdminDto } from '../dtos/loginAdminDto';
+
+export const adminData: CreateAdminDto = {
   username: 'Jololo',
   password: 'lalax$123',
   phoneNumber: '0788823129',
-  role: 'manager',
+  role: AdminRole.ADMIN,
+};
+export const adminLoginData: LoginAdminDto = {
+  username: 'Jololo',
+  password: 'lalax$123',
+};
+export const adminLoginFailUsernameData: LoginAdminDto = {
+  username: 'Joolo',
+  password: 'lalax$123',
+};
+export const adminLoginFailPasswordData: LoginAdminDto = {
+  username: 'Jololo',
+  password: 'lalax$12',
 };
