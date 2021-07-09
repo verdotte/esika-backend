fe287f55466cd0206ab12e3e6e2ba5d019929437
Desire Kaleba <desirekaleba@gmail.com>
2021-07-09 22:03:58 +0300

    feat: add test db url

--- ormconfig.ts
+++ ormconfig.ts
@@ -10,18 +10,25 @@ const {
   DB_PORT,
   DEV_DB,
   PROD_DB,
+  TEST_DB,
   NODE_ENV,
 } = process.env;
 
 const dir = NODE_ENV === 'production' ? 'build' : 'src';
 
+const database = NODE_ENV === 'production'
+  ? PROD_DB
+  : NODE_ENV === 'development'
+  ? DEV_DB
+  : TEST_DB;
+  
 const config: ConnectionOptions = {
   type: 'mysql',
   host: DB_HOST,
   port: Number(DB_PORT),
   username: DB_USER,
   password: DB_PASSWORD,
-  database: NODE_ENV === 'production' ? PROD_DB : DEV_DB,
+  database,
   synchronize: NODE_ENV === 'production' ? false : true,
   logging: false,
   entities: [`${dir}/database/entity/**/*.{ts,js}`],
