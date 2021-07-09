386dbd4e8297009b38d9927eac60db636a0d006a
Desire Kaleba <desirekaleba@gmail.com>
2021-07-09 22:08:49 +0300

    fix: ./ is not defined by exports

--- src/plugins/twilo/twilioService.ts
+++ src/plugins/twilo/twilioService.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv/';
+import dotenv from 'dotenv';
 import { twilioClient } from './twilio';
 import { ITwilioResponse } from '../../interfaces/twilio.interface';
 dotenv.config();
