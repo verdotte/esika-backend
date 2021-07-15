8b355b92b382278b0b0f09f5a62688f6b7c688bf
Desire Kaleba <desirekaleba@gmail.com>
2021-07-15 17:57:27 +0300

    feat: add update user info controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,6 +3,8 @@ import { ResponseUtil } from '../utils';
 import { UserService } from '../database/services';
 import { OK } from '../constants/statusCodes';
 import { IRequestWithUser } from '../interfaces/requestWithUser.interface';
+import { UpdateUserDto } from '../dtos/updateUserDto'; 
+import { updated } from '../constants/responseMessages';
 
 /**
  * User Controller
@@ -29,7 +31,7 @@ export class UserController {
    * @param {IRequestWithUser} req
    * @param {Response} res
    * @returns {object} user payload
-   * @memberof AuthController
+   * @memberof UserController
    */
   getProfile = async (
     req: IRequestWithUser,
@@ -52,7 +54,7 @@ export class UserController {
    * @param {Request} req
    * @param {Response} res
    * @returns {array} users payload
-   * @memberof AuthController
+   * @memberof UserController
    */
   getAllHost = async (req: Request, res: Response): Promise<Response> => {
     const userList = await this.userService.findByUserType('host');
@@ -62,5 +64,28 @@ export class UserController {
       data: { host: userList },
       res,
     });
+  };
+  /**
+   * Update info
+   * @author Desire Kaleba
+   * @since 0.001
+   *
+   * @param {Request} req
+   * @param {Response} res
+   * @returns {array} users payload
+   * @memberof UserController
+   */
+   updateInfo = async (req: Request, res: Response): Promise<Response> => {
+     const userId = parseInt(req.params.userId, 10);
+     const updatedInfo: UpdateUserDto = req.body;
+
+     const updatedUser = await this.userService.update(userId, updatedInfo);
+    
+     return this.responseUtil.success({
+        statusCode: OK,
+        message: updated('Your information'),
+        data: updatedUser,
+        res,
+     });
   };
 }
