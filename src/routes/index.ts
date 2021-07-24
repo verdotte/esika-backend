beaecba519029362f35dca1fc6e9bca4ef602d40
Desire Kaleba <desirekaleba@gmail.com>
2021-07-24 16:20:22 +0300

    chore: add api docs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import { AuthRoute } from './authRoute';
 import { AdminRoute } from './adminRoute';
 import { UserRoute } from './userRoute';
 import { PropertyRoute } from './propertyRoute';
+import { DocsRoute } from './docsRoute';
 import {
   authController,
   adminController,
@@ -14,10 +15,12 @@ const authRoute = new AuthRoute(authController);
 const adminRoute = new AdminRoute(adminController);
 const userRoute = new UserRoute(userController);
 const propertyRoute = new PropertyRoute(propertyController);
+const docsRoute = new DocsRoute();
 
 export const routes: IRoute[] = [
   authRoute,
   adminRoute,
   userRoute,
   propertyRoute,
+  docsRoute
 ];
