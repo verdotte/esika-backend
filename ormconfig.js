0b41d089d43477b1bcd71c875a42714ede460a6a
verdotte <ututuv@gmail.com>
2021-07-24 03:40:00 +0300

    fix: db connection failure

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
 const dotenv = require('dotenv');
 
 dotenv.config();
--- src/database/initializeDB.ts
+++ src/database/initializeDB.ts
@@ -4,11 +4,10 @@ import logger from '../utils/logger';
 export const initializeDB = async (): Promise<void> => {
   try {
     const conn = await createConnection();
-    console.log(
+    logger.info(
       `Database: ${conn.options.database} is successfully initialized`,
     );
   } catch (error) {
-    console.log('>>>', error);
-    console.log(`Database failed to connect ${error.message}`);
+    logger.info(`Database failed to connect ${error.message}`);
   }
 };
